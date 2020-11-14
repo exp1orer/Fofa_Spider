ace63f830b4cc2d27964e1608f6ddacbe2e164c2
exp1orer <43846937@qq.com>
2020-11-14 14:48:22 +0800

    Update Fofa_Spider

--- Fofa_Spider.py
+++ Fofa_Spider.py
@@ -62,8 +62,11 @@ if __name__ == '__main__':
   output = input("请选择是否只输出存活url[y/N导出全部] ")
   results = get_fofa(api_url)
   loop = asyncio.get_event_loop()  # 获取当前事件循环, 如果没有则创建并将其设为当前时间循环
-  if len(results) > 0: # 判断是否有url
-    tasks = [asyncio.ensure_future(check_url(url, output)) for url in results]  # ensure_future(obj), 如果obj是一个协程则加入执行计划
-    loop.run_until_complete(asyncio.wait(tasks))  # wait(aws) aws需是可迭代且不为空对象, 返回done和panding集合. run_until_complete(future)运行和停止事件循环, 返回 Future 的结果 或者引发相关异常.
-  else:
-    print("Error:没有搜索到结果!")
+  try:
+    if results: # 判断是否有url
+      tasks = [asyncio.ensure_future(check_url(url, output)) for url in results]  # ensure_future(obj), 如果obj是一个协程则加入执行计划
+      loop.run_until_complete(asyncio.wait(tasks))  # wait(aws) aws需是可迭代且不为空对象, 返回done和panding集合. run_until_complete(future)运行和停止事件循环, 返回 Future 的结果 或者引发相关异常.
+    else:
+      print("Error:没有搜索到结果!")
+  except Exception as e:
+    print(e)
